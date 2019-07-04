f6d43859cce3fc6fcd17d96489a7f4f840a78c49
Kseniya Kuzmina <kseniya.kuzmina@devexpress.com>
2019-07-04 14:33:13 +0300

    Upgrate to 18.1

--- CS/MultiPaneChart/Form1.Designer.cs
+++ CS/MultiPaneChart/Form1.Designer.cs
@@ -86,10 +86,9 @@ namespace MultiPaneChart
             // 
             // barStaticItem1
             // 
-            this.barStaticItem1.Caption = "Ser Max Series Value";
+            this.barStaticItem1.Caption = "Max Series Value";
             this.barStaticItem1.Id = 1;
             this.barStaticItem1.Name = "barStaticItem1";
-            this.barStaticItem1.TextAlignment = System.Drawing.StringAlignment.Near;
             // 
             // barEditItem1
             // 
@@ -106,18 +105,51 @@ namespace MultiPaneChart
             new DevExpress.XtraEditors.Controls.EditorButton()});
             this.repositoryItemSpinEdit1.Name = "repositoryItemSpinEdit1";
             // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(634, 26);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 445);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(634, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 26);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 419);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(634, 26);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 419);
+            // 
             // panel1
             // 
             this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.chartControl1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Location = new System.Drawing.Point(0, 26);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(626, 420);
+            this.panel1.Size = new System.Drawing.Size(634, 419);
             this.panel1.TabIndex = 4;
             // 
             // chartControl1
             // 
+            this.chartControl1.Legend.Name = "Default Legend";
             this.chartControl1.Location = new System.Drawing.Point(12, 6);
             this.chartControl1.Name = "chartControl1";
             this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
@@ -129,7 +161,7 @@ namespace MultiPaneChart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(626, 445);
+            this.ClientSize = new System.Drawing.Size(634, 445);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.barDockControlLeft);
             this.Controls.Add(this.barDockControlRight);
@@ -143,6 +175,7 @@ namespace MultiPaneChart
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
--- CS/MultiPaneChart/Form1.cs
+++ CS/MultiPaneChart/Form1.cs
@@ -1,25 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 using DevExpress.XtraCharts;
+using DevExpress.XtraEditors;
+using System;
+using System.Drawing;
 
-namespace MultiPaneChart
-{
-    public partial class Form1 : Form
-    {
+namespace MultiPaneChart {
+    public partial class Form1 : XtraForm {
         private int seriesName = 0;
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
         }
 
-        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-        {
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
             chartControl1.Visible = true;
             int arraySize = 12;
             int maxValue = 0;
@@ -27,38 +18,34 @@ namespace MultiPaneChart
             object[] workValues = new object[arraySize];
             string[] monthStrings = new string[arraySize];
             Random generator = new Random();
-            for (int i = 0; i < arraySize; i++)
-            {
+            for (int i = 0; i < arraySize; i++) {
                 workValues[i] = generator.NextDouble() * maxValue;
                 monthStrings[i] = DateTime.Now.AddMonths(-i).ToString("yyyy-MM");
             }
-            AddSeriesPaneToChart(string.Format("Series:{0}", seriesName), workValues, monthStrings, Color.Aqua);
+            AddSeriesPaneToChart(string.Format("Series:{0}", seriesName), workValues, monthStrings, Color.Coral);
             seriesName += 1;
         }
-        private Series GetSeries(string name, Color color, object[] array, string[] months)
-        {
+        private Series GetSeries(string name, Color color, object[] array, string[] months) {
             Series series = new Series(name, ViewType.Spline);
             series.ArgumentScaleType = ScaleType.Qualitative;
             ((SplineSeriesView)series.View).LineMarkerOptions.Kind = MarkerKind.Circle;
-            ((SplineSeriesView)series.View).LineMarkerOptions.Visible = true;
+            ((SplineSeriesView)series.View).MarkerVisibility = DevExpress.Utils.DefaultBoolean.True;
 
             ((SplineSeriesView)series.View).LineStyle.DashStyle = DashStyle.Solid;
             ((SplineSeriesView)series.View).Color = color;
 
-            series.Label.Visible = false;
+            series.LabelsVisibility = DevExpress.Utils.DefaultBoolean.True;
 
 
             int i = 0;
-            foreach (object value in array)
-            {
+            foreach (object value in array) {
                 series.Points.Add(new SeriesPoint(months[i], value));
                 i += 1;
             }
             return series;
         }
 
-        public void AddSeriesPaneToChart(string textFormatString, object[] values, string[] labelStrings, Color color)
-        {
+        public void AddSeriesPaneToChart(string textFormatString, object[] values, string[] labelStrings, Color color) {
             object[] array = values;
             string[] months = labelStrings;
             Series series = GetSeries(textFormatString, color, array, months);
@@ -69,8 +56,7 @@ namespace MultiPaneChart
             XYDiagram diag = chartControl1.Diagram as XYDiagram;
             int axesPosition = diag.SecondaryAxesY.Add(new SecondaryAxisY());
 
-            if (seriesPosition > 0)
-            {
+            if (seriesPosition > 0) {
 
                 XYDiagramPane pane = new XYDiagramPane();
                 pane.SizeMode = PaneSizeMode.UseWeight;
@@ -82,19 +68,19 @@ namespace MultiPaneChart
                 view.Pane = pane;
 
             }
-            else
-            {
-                diag.SecondaryAxesY[seriesPosition].Visible = false;
+            else {
+                diag.SecondaryAxesY[seriesPosition].Visibility = DevExpress.Utils.DefaultBoolean.False;
                 view.AxisX.Label.Angle = 90;
             }
 
-            diag.EnableZooming = true;
-            diag.EnableScrolling = true;
+            diag.EnableAxisXScrolling = true;
+            diag.EnableAxisXZooming = true;
+            diag.EnableAxisYScrolling = true;
+            diag.EnableAxisYZooming = true;
 
             chartControl1.Height += 400;
         }
-        private void Form1_Resize(object sender, EventArgs e)
-        {
+        private void Form1_Resize(object sender, EventArgs e) {
             chartControl1.Width = this.ClientSize.Width;
         }
     }
